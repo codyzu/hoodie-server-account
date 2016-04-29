86c6e7a9c2724b862662f088e45afbbebb8694f1
codyzu <cody.zuschlag@gmail.com>
2016-04-29 18:37:23 +0200

    try to udpate session

--- api/utils/couchdb-update-session-id-name.js
+++ api/utils/couchdb-update-session-id-name.js
@@ -3,8 +3,10 @@ module.exports = updateSessionIdName
 var calculateSessionId = require('couchdb-calculate-session-id')
 var decodeSessionId = require('./couchdb-decode-session-id')
 
-function updateSessionIdName (secret, salt, sessionId, name) {
+function updateSessionIdName (secret, sessionId, doc) {
   var session = decodeSessionId(sessionId)
+  var name = doc.name
+  var salt = doc.salt
   var time = session.time
   return calculateSessionId(name, salt, secret, time)
 }
